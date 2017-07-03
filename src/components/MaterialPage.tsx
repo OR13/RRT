581e6c28690a59de27d6783089abf23843fb1cfa
Orie Steele <osteele@athenahealth.com>
2017-07-03 15:38:53 -0500

    Added Material UI example

--- src/components/MaterialPage.tsx
+++ src/components/MaterialPage.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
 
+// import RaisedButton from 'material-ui/RaisedButton';
+
+// import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// import {MuiThemeProvider, lightBaseTheme} from 'material-ui/styles';
+
+// const lightMuiTheme = getMuiTheme(lightBaseTheme);
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { MuiThemeProvider, lightBaseTheme } from 'material-ui/styles';
+
+const lightMuiTheme = getMuiTheme(lightBaseTheme);
+
+import RaisedButton from 'material-ui/RaisedButton';
+
 export default class MaterialPage extends React.Component<any, any> {
-  render () {
+  render() {
     return (
       <div>
-       Hello World
+        <MuiThemeProvider muiTheme={lightMuiTheme}>
+          <RaisedButton label="Default" />
+        </MuiThemeProvider>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 // import AppConnected from './containers/App';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import './index.css';
-import {store, history} from './store/store';
+import { store, history } from './store/store';
 
-import { ConnectedRouter,  } from 'react-router-redux';
+import { ConnectedRouter, } from 'react-router-redux';
 import { Route } from 'react-router';
+
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 // Now you can dispatch navigation actions from anywhere!
 // store.dispatch(push('/foo'))
@@ -19,8 +26,8 @@ ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div>
-        <Route exact={true} path="/" component={StarWarsPage}/>
-        <Route path="/material" component={MaterialPage}/>
+        <Route exact={true} path="/" component={StarWarsPage} />
+        <Route path="/material" component={MaterialPage} />
       </div>
     </ConnectedRouter>
   </Provider>,
